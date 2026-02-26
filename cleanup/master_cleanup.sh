a20543b53b33a3c5a744a49928d480ab0f9ecb95
Arpita Naik <arpitan462@gmail.com>
2026-02-26 09:24:53 +0530

    Removed all files to reset repository

--- cleanup/master_cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "===== HPC CLEANUP START ====="
-
-bash cleanup/remove_python_env.sh
-bash cleanup/remove_modules.sh
-bash cleanup/remove_slurm.sh
-
-echo "===== CLEANUP COMPLETE ====="
--- cleanup/remove_python_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -d "$HOME/hpc_python_env" ]; then
-    echo "Removing Python virtual environment..."
-    rm -rf $HOME/hpc_python_env
-else
-    echo "No Python virtual environment found."
-fi
