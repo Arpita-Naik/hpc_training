320a5d288f82cda906d5e20672296d0b2d588128
Arpita Naik <arpitan462@gmail.com>
2026-05-11 11:04:12 +0530

    Remove comment about array initializationch

--- task_1.c
+++ task_1.c
@@ -19,7 +19,6 @@ int main(int argc,char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
     
-    //rank 0 initilizes the array(temp)
     if(rank==0)
     {
         printf("Process 0 is initializing the array...\n");
